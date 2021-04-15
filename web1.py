9990671be713e3b9d1a153dfff82fc5af2845eb0
papasmf1 <papasmf1@gmail.com>
2021-04-15 16:32:41 +0900

    두번째

--- web1.py
+++ web1.py
@@ -6,4 +6,14 @@ page = open("c:\\work\\test01.html", "rt", encoding="utf-8").read()
 #검색이 용이한 객체
 soup = BeautifulSoup(page, "html.parser")
 #문서 전체를 보기
-print( soup.prettify() )
+#print( soup.prettify() )
+#find_all("태그")
+#print( soup.find_all("p") )
+#<p class="outer-text"> 필터링 
+#print( soup.find_all("p", class_="outer-text") ) 
+
+#검색을 해서 태그 안쪽의 컨텐츠 
+for tag in soup.find_all("p", class_="outer-text"):
+    print( tag.text )
+
+    
